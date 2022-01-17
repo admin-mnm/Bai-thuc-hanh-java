35e127299110f60951068e2f94a3ddeb9084fae4
ta_tuan_anh_2506 <62741727+ta-tuan-anh@users.noreply.github.com>
2022-01-17 17:04:21 +0700

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,11 +1,5 @@
-import java.util.Scanner;
-public class App{
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String strHoTen ="";
-        System.out.println("Nhap ten: ");
-        String yourName = scanner.nextLine();
-        strHoTen = scanner.nextLine();
-        System.out.println("Xin Chao : " + yourName);
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
     }
 }
